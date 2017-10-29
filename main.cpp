4e25e5525c8f8741d1db2ca88437f7cf051c8167
Gijs <g.m.entius@gmail.com>
2017-10-29 10:44:10 +0100

    building a new solution

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * BesturingsEenheid.cpp
+ *
+ * Created: 29-10-2017 10:00:59
+ * Author : Gijs
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Code to let a control unit run independently */
+    while (1) 
+    {
+		
+    }
+}
+
