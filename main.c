1397202250d4f8b966b70a14138e8b3e25e527d8
Gijs <g.m.entius@gmail.com>
2017-10-29 10:47:17 +0100

    m

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * project_arduino.c
+ *
+ * Created: 29-10-2017 10:45:52
+ * Author : Gijs
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
