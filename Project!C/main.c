58126b4f437e32fb8d2713bc55c2657eebacae64
Daggh <d.g.hoogendorp@st.hanze.nl>
2017-10-30 13:24:17 +0100

    sensoren

---
+++ Project!C/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project!C.c
+ *
+ * Created: 30-10-2017 13:01:48
+ * Author : Dagmar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Project!C/pinIO.h
@@ -0,0 +1,19 @@
+#ifndef PINIO_
+#define PINIO_
+
+#include <avr/io.h>
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+#define IN 0
+#define OUT 1
+
+void digital_config(int pin, int value);
+void digital_write(int pin, int value);
+int digital_read(int pin);
+void adc_config();
+uint16_t adc_read(uint8_t adcx);
+
+
+#endif PINIO_
