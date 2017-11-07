d540b0c6b990b86e82b6b929ce8d76928c1c9f3e
SteveHilly <sp.hilberts@outlook.com>
2017-11-07 14:55:58 +0100

    added serial connection class with functions

--- centrale/Serial.py
+++ centrale/Serial.py
@@ -18,15 +18,29 @@ class SerialConnection:
         self.baudrate = baudrate
         self.port = port
         self.connection = serial.Serial(baudrate=baudrate, port=port)  # setting up connection with port and baudrate.
-
-    def open(self):
         self.connection.open()
 
-    def close(self):
-        self.connection.close()
+    def portcheck(self):
+        print("Connected to: " + self.connection.port)  # prints to what port you are connected
 
-    def send(self):
-        pass
+    def open(self):
+        self.connection.open()  # opens the connection
+
+    def close(self):
+        self.connection.close()  # closes the connection
+
+    def send(self, command, value):
+        pass  # sending 8 bits at a time, waiting for another function to use here
 
     def receive(self):
-        pass
+        data = []  # init the list where the received data will be saved
+        block_part = self.connection.read(size=1)  # reading the data in parts of 8 bits / 1 byte
+        data.append(block_part)  # adding received data to the list
+        block_part = self.connection.read(size=1)  # reading the data in parts of 8 bits / 1 byte
+        data.append(block_part)  # adding received data to the list
+        return data
+
+
+
+
+
